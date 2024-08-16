acb4b830286b0b139b0d7e037c31fdf47b59dc29
Greenhouse <greenhouse8161@gmail.com>
2024-08-16 09:51:34 +0900

    first

---
+++ BlazorToDoList.App/Startup.cs
@@ -0,0 +1,61 @@
+using BlazorToDoList.App.Data;
+using BlazorToDoList.App.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace BlazorToDoList.App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor()
+                .AddCircuitOptions(options =>
+                {
+                    //can toggle detailed errors on or off from app settings
+                    options.DetailedErrors = Convert.ToBoolean(Configuration["DetailedErrors"]);
+                });
+
+            services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<IFileService, FileService>();
+            services.AddScoped<IToDoService, ToDoService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
